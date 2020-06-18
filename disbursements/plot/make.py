0e252a83f9dd4ea142556d73e4cfd51f0955c136
Jack Walton <jwalton3141@gmail.com>
2020-06-18 13:23:10 +0100

    Plot overall / county wide expenditure

--- disbursements/plot/make.py
+++ disbursements/plot/make.py
@@ -9,6 +9,15 @@ from plot.pretty import make_fig_ax, savefig
 
 def custom(data, category, entry):
     """Plot expenditure of entry in category."""
+    # Plot per parish
+    __custom_parish(data, category, entry)
+
+    # Global plot over summed parishes
+    __custom_total(data, category, entry)
+
+
+def __custom_parish(data, category, entry):
+    """Plot expenditure of entry in category per parish."""
     # Compute expenditure
     entry_total, entry_percent = __compute_expenditure(data, category, entry)
 
@@ -20,7 +29,52 @@ def custom(data, category, entry):
     __tabulate_summary(entry_total, entry)
 
 
-def __compute_expenditure(data, category, entry):
+def __custom_total(data, category, entry):
+    """Plot expenditure of entry in category over all parishes."""
+    # Compute expenditure
+    entry_total, entry_percent = __compute_expenditure(data, category, entry, by_parish=False)
+
+    __plot_overall(entry_total.Total, entry)
+    __plot_overall_percent(entry_percent, entry)
+
+
+def __plot_overall(data, entry):
+    fig, ax = make_fig_ax()
+    __plot(ax, data)
+
+    # Titles and labels
+    ax.set_title('Annual {} expenditure'.format(entry))
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Expenditure in pence')
+
+    # Save plot
+    savefig(fig,
+            '{}_expenditure'.format(entry),
+            'overall',
+            entry,
+            bbox_inches='tight')
+
+
+def __plot_overall_percent(data, entry):
+    fig, ax = make_fig_ax()
+    __plot(ax, data)
+
+    # Titles and labels
+    # Title and labels
+    ax.set_title('Annual {} expenditure as a '
+                 'percentage of total expenditure.'.format(entry))
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Percentage of total expenditure')
+
+    # Save plot
+    savefig(fig,
+            '{}_percent_expenditure'.format(entry),
+            'overall',
+            entry,
+            bbox_inches='tight')
+
+
+def __compute_expenditure(data, category, entry, by_parish=True):
     """Compute total and proportional expenditure."""
     data = data.copy()
     data = make_year_col(data)
@@ -29,8 +83,12 @@ def __compute_expenditure(data, category, entry):
     entry_data = data[data[category] == entry]
 
     # Sum expenditure by year and parish
-    entry_groupby = entry_data.groupby(['Parish_Name', 'Year']).sum()
-    data_groupby = data.groupby(['Parish_Name', 'Year']).sum()
+    if by_parish:
+        entry_groupby = entry_data.groupby(['Parish_Name', 'Year']).sum()
+        data_groupby = data.groupby(['Parish_Name', 'Year']).sum()
+    else:
+        entry_groupby = entry_data.groupby(['Year']).sum()
+        data_groupby = data.groupby(['Year']).sum()
 
     # Express expenditure in pence
     entry_groupby = total_from_pds(entry_groupby)
@@ -56,7 +114,7 @@ def __plot_parishes(data, entry, plot_fn):
 
 def __plot_total(fig, ax, data, entry, parish):
     """Plot expenditure of parish on entry over time."""
-    __plot(ax, data, parish)
+    __plot(ax, data.loc[parish])
 
     # Titles and labels
     ax.set_title('{}: Annual {} expenditure'.format(parish, entry))
@@ -73,10 +131,10 @@ def __plot_total(fig, ax, data, entry, parish):
 
 def __plot_percent(fig, ax, data, entry, parish):
     """Plot proportional expenditure of parish on entry over time."""
-    __plot(ax, data, parish)
+    __plot(ax, data.loc[parish])
 
     # Title and labels
-    ax.set_title('{}: {} expenditure as a '
+    ax.set_title('{}: Annual {} expenditure as a '
                  'percentage of total expenditure.'.format(parish, entry))
     ax.set_xlabel('Year')
     ax.set_ylabel('Percentage of total expenditure')
@@ -89,19 +147,17 @@ def __plot_percent(fig, ax, data, entry, parish):
             bbox_inches='tight')
 
 
-def __plot(ax, data, parish):
+def __plot(ax, data):
     """Plot parish data over time."""
     # Line plot of year vs. expenditure
-    ax.plot(data.loc[parish].reset_index().Year,
-            data.loc[parish])
+    ax.plot(data.reset_index().Year, data)
 
     # Scatter plot of year vs. expenditure
-    ax.scatter(data.loc[parish].reset_index().Year,
-               data.loc[parish])
+    ax.scatter(data.reset_index().Year, data)
 
     # Set xlimits
-    ax.set_xlim(data.loc[parish].reset_index().Year.min(),
-                data.loc[parish].reset_index().Year.max())
+    ax.set_xlim(data.reset_index().Year.min(),
+                data.reset_index().Year.max())
 
 
 def __tabulate_summary(df, entry):
