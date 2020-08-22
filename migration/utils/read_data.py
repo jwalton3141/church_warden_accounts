7a81d3e23acfd6081f608702f640bbade1eb92c1
Jack Walton <jwalton3141@gmail.com>
2020-08-22 15:29:06 +0100

    Tidy and comment

--- migration/utils/read_data.py
+++ migration/utils/read_data.py
@@ -7,57 +7,42 @@ import numpy as np
 import os
 import os.path as path
 import re
-import sys
 
 from utils.get_locs import selenium_lookup
 
 
-def read_population(file_name=None):
+def read_excel(file_name, **kwargs):
+    """Load excel spreadsheet from data directory."""
+    # Ensure filename ends with xlsx extension
+    file_name += (not file_name.endswith('.xlsx')) * '.xlsx'
+
+    # Construct path to data directory
+    data_dir_path = path.join(
+                        path.dirname(
+                            path.dirname(
+                                path.realpath(__file__))),
+                        'data')
+
+    # List the files in the data directory
+    files = os.listdir(data_dir_path)
+
+    # Check that the requested file exists
+    assert file_name in files, \
+            "file '{}' not found in directory '{}'".format(file_name,
+                                                           data_dir_path)
+
+    # Read and return
+    return pd.read_excel(file_name, **kwargs)
+
+
+def load_population():
     """Load the population changes data."""
-
-    # Get the path to the data directory
-    data_dir_path = path.join(path.dirname(
-                              path.dirname(
-                              path.realpath(__file__))),
-                              'data')
-
-    if not file_name:
-        # Get all the files in the data directory
-        files = os.listdir(data_dir_path)
-
-        if 'Population_Change.xlsx' in files:
-            file_name = path.join(data_dir_path, 'Population_Change.xlsx')
-        else:
-            print("Could not find spreadsheet with name "
-                  "'Population_Change.xlsx' in folder "
-                  "{}.".format(data_dir_path))
-
-    data = pd.read_excel(file_name, sheet_name=None, index_col=0)['Sheet1']
-
-    return data
+    return read_excel('Population_Change', {'index_col': 0})
 
 
-def read_marriages(file_name=None):
-    """Load marriages data."""
-    # Get the path to the data directory
-    data_dir_path = path.join(path.dirname(
-                              path.dirname(
-                              path.realpath(__file__))),
-                              'data')
-
-    if not file_name:
-        # Get all the files in the data directory
-        files = os.listdir(data_dir_path)
-
-        if 'Marriage_Migration.xlsx' in files:
-            file_name = path.join(data_dir_path, 'Marriage_Migration.xlsx')
-        else:
-            print("Could not find spreadsheet with name "
-                  "'Marriage_Migration.xlsx' in folder "
-                  "{}.".format(data_dir_path))
-
+def load_marriages():
     # Load data
-    data = pd.read_excel(file_name, sheet_name=None)
+    data = read_excel('Marriage_Migration', {'sheet_name': None})
 
     # Tidy data
     data = tidy_marriages(data)
@@ -65,7 +50,7 @@ def read_marriages(file_name=None):
     # Count occurances of pairs
     pairs, counts = np.unique(data, axis=0, return_counts=True)
 
-    # Get locations 
+    # Get locations
     locations = lookup_locs(data)
 
     return pairs, counts, locations
@@ -74,9 +59,11 @@ def read_marriages(file_name=None):
 def tidy_marriages(data):
     """Extract the groom and bride parish from spreadsheet and clean."""
     # Extract parsishes of Groom (dropping nans and empty entries)
-    x = [list(values['Parish']) for values in data.values() if np.any(values['Parish'])]
+    x = [list(values['Parish']) for values in data.values()
+            if np.any(values['Parish'])]
     # Extract parished of Bride (dropping nans and empty entries)
-    y = [list(values['Parish.1']) for values in data.values() if np.any(values['Parish.1'])]
+    y = [list(values['Parish.1']) for values in data.values()
+            if np.any(values['Parish.1'])]
 
     # Flatten list of lists structure
     x = [item for sublist in x for item in sublist]
@@ -94,37 +81,38 @@ def lookup_locs(data):
     # Get list of parishes referenced
     parishes = np.unique(data)
 
-    # Get path to output directory
+    # Construct the path to the output directory
     out_dir = path.join(
-                path.dirname(
-                path.dirname(
-                    path.realpath(__file__))),
-                'output')
-    # Get path to spreadsheet
+                  path.dirname(
+                      path.dirname(
+                          path.realpath(__file__))),
+                  'output')
+
+    # Construct path to lookup table
     path_to_table = path.join(out_dir, 'parish_locations.xlsx')
 
+    # If lookup table does not exist
     if not path.isfile(path_to_table):
-        # Lookup all listed parishes
+        # Perform lookup 
         df = selenium_lookup(parishes)
         # Write lookup table
         df.to_excel(path_to_table)
     else:
-        # Read in what we've already got
+        # Read in current lookup table
         df = pd.read_excel(path_to_table, index_col=0)
 
-        # Construct boolean which indicates whether a parish is in the lookup table
-        # regex drops problems with titles including brackets
+        # Construct boolean indicating which parishes already exist in lookup
+        # regex drops problems with titles which include parentheses
         located = np.array([np.any(df.Title.str.contains(parish, regex=False))
                             for parish in parishes])
 
-        # If there are any parishes in data which aren't in df, look them up
+        # Lookup parishes not already listed
         if len(parishes[~located]) >0:
-            # Lookup locations of parishes not listed in current table
+            # New entries
             new_locs = selenium_lookup(parishes[~located])
 
-            # Append missing parishes
+            # Append new entries
             df = df.append(new_locs, ignore_index=True)
-
             # Sort alphabetically
             df = df.sort_values('Title')
 
@@ -139,6 +127,3 @@ def lookup_locs(data):
 
     return df
 
-
-if __name__ == "__main__":
-    read_marriages()
