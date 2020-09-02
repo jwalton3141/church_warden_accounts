be6429f78656bf9a00858d3ab736733cc7924aa4
Jack Walton <jwalton3141@gmail.com>
2020-09-02 23:04:36 +0100

    begin tidy

--- migration/plot/population_change.py
+++ migration/plot/population_change.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-from utils.read_data import read_population
+from migration.utils.read_data import read_population
 from plot.pretty import make_fig_ax, savefig
 
 data = read_population()
@@ -15,162 +15,87 @@ data = read_population()
 groupby = data.groupby('Year').sum()
 groupby = groupby.reset_index(level=0)
 
-fig, ax = make_fig_ax()
-ax.plot(groupby.Year,
-        groupby['Baptism Total'] - groupby['Burial Total'],
-        c='C2', marker='o')
-ax.hlines(0, groupby.Year.min(), groupby.Year.max(),
-          color='k', alpha=0.65, linestyle='--', linewidth=1.5)
-ax.set_xlabel('Year')
-ax.set_ylabel('Annual population change (baptisms - burials)')
-ax.set_xlim(groupby.Year.min(), groupby.Year.max())
 
-fig.tight_layout(pad=0)
-fig.savefig('output/annual_change.png')
+def make_plot(cols, ylabel, save_name, ratio=False, cumsum=False):
+    fig, ax = make_fig_ax()
 
+    if ratio:
+        y1 = groupby[col[0]] / groupby[col[1]]
+        hline = 1
 
-fig, ax = make_fig_ax()
-ax.plot(groupby.Year,
-        groupby['Baptism Total'] / groupby['Burial Total'],
-        c='C2', marker='o')
-ax.hlines(1, groupby.Year.min(), groupby.Year.max(),
-          color='k', alpha=0.65, linestyle='--', linewidth=1.5)
-ax.set_xlabel('Year')
-ax.set_ylabel('Ratio of annual baptisms to burials')
-ax.set_xlim(groupby.Year.min(), groupby.Year.max())
+        if len(cols) > 2:
+            y2 = groupby[col[2]] / groupby[col[3]]
+        else:
+            y2 = None
+    else:
+        y1 = groupby[col[0]] - groupby[col[1]]
+        hline = 0
 
-fig.tight_layout(pad=0)
-fig.savefig('output/annual_ratio.png')
+        if len(cols) > 2:
+            y2 = groupby[col[2]] - groupby[col[3]]
+        else:
+            y2 = None
 
+        if cumsum:
+            y1 = np.cumsum(y1)
+            if y2 is not None:
+                y2 = np.cumsum(y2)
+            hline = None
 
-fig, ax = make_fig_ax()
-ax.plot(groupby.Year,
-        np.cumsum(groupby['Baptism Total'] - groupby['Burial Total']),
-        c='C2', marker='o')
-ax.set_xlabel('Year')
-ax.set_ylabel('Cumulative population change')
-ax.set_xlim(groupby.Year.min(), groupby.Year.max())
+    ax.plot(groupby.Year, y1,
+            groupy.Year, y2)
 
-fig.tight_layout(pad=0)
-fig.savefig('output/cumulative_change.png')
+    ax.set_xlabel('Year')
+    ax.set_ylabel(ylabel)
 
+    ax.hlines(hline, groupby.Year.min(), groupby.Year.max(),
+              color='k', alpha=0.65, linestyle='--', linewidth=1.5)
 
-fig, ax = make_fig_ax()
-ax.plot(groupby.Year,
-        groupby['Boy Baptism'] - groupby['Boy Burials'],
-        c='C0', marker='o')
-ax.set_xlabel('Year')
-ax.set_ylabel('Male population change (baptisms - burials)')
-ax.hlines(0, groupby.Year.min(), groupby.Year.max(),
-          color='k', alpha=0.65, linestyle='--', linewidth=1.5)
-ax.set_xlim(groupby.Year.min(), groupby.Year.max())
+    ax.set_xlim(groupby.Year.min(), groupby.Year.max())
 
-fig.tight_layout(pad=0)
-fig.savefig('output/annual_male_change.png')
+    fig.tight_layout(pad=0)
+
+    fig.savefig(path.join('output', save_name))
 
 
-fig, ax = make_fig_ax()
-ax.plot(groupby.Year,
-        groupby['Girl Baptism'] - groupby['Girl Burials'],
-        c='C6', marker='o')
-ax.set_xlabel('Year')
-ax.set_ylabel('Female population change (baptisms - burials)')
-ax.hlines(0, groupby.Year.min(), groupby.Year.max(),
-          color='k', alpha=0.65, linestyle='--', linewidth=1.5)
-ax.set_xlim(groupby.Year.min(), groupby.Year.max())
+make_plot(['Baptism Total', 'Burial Total'],
+          'Annual population change (baptisms - burials)',
+          'annual_change.png')
 
-fig.tight_layout(pad=0)
-fig.savefig('output/annual_female_change.png')
+make_plot(['Baptism Total', 'Burial Total'],
+          'Ratio of annual baptisms to burials',
+          'annual_ratio.png',
+          ratio=True)
 
+make_plot(['Baptism Total', 'Burial Total'],
+          'Cumulative population change',
+          'cumulative_change.png',
+          cumsum=True)
 
-fig, ax = make_fig_ax()
-ax.plot(groupby.Year,
-        groupby['Boy Baptism'] - groupby['Boy Burials'],
-        c='C0', label='Male', marker='o')
-ax.plot(groupby.Year,
-        groupby['Girl Baptism'] - groupby['Girl Burials'],
-        c='C6', label='Female', marker='o')
-ax.hlines(0, groupby.Year.min(), groupby.Year.max(),
-          color='k', alpha=0.65, linestyle='--', linewidth=1.5)
-ax.set_xlabel('Year')
-ax.set_ylabel('Annual population change (baptisms - burials)')
-ax.set_xlim(groupby.Year.min(), groupby.Year.max())
-ax.legend(loc=0)
+make_plot(['Boy Baptism', 'Boy Burials'],
+          'Male population change (baptisms - burials)',
+          'annual_male_change.png')
 
-fig.tight_layout(pad=0)
-fig.savefig('output/annual_gender_change.png')
+make_plot(['Girl Baptism', 'Girl Burials'],
+          'Female population change (baptisms - burials)',
+          'annual_female_change.png')
 
+make_plot(['Boy Baptism', 'Boy Burials', 'Girl Baptism', 'Girl Burials'],
+          'Annual population change (baptisms - burials)',
+          'annual_gender_change.png')
 
-fig, ax = make_fig_ax()
-ax.plot(groupby.Year,
-        np.cumsum(groupby['Boy Baptism'] - groupby['Boy Burials']),
-        c='C0', label='Male', marker='o')
-ax.plot(groupby.Year,
-        np.cumsum(groupby['Girl Baptism'] - groupby['Girl Burials']),
-        c='C6', label='Female', marker='o')
-ax.set_xlabel('Year')
-ax.set_ylabel('Cumulative population change')
-ax.set_xlim(groupby.Year.min(), groupby.Year.max())
-ax.legend(loc=0)
+make_plot(['Boy Baptism', 'Boy Burials', 'Girl Baptism', 'Girl Burials'],
+          'Cumulative population change',
+          'cumulative_gender_change.png',
+          cumsum=True)
 
-fig.tight_layout(pad=0)
-fig.savefig('output/cumulative_gender_change.png')
+make_plot(['Boy Baptism', 'Boy Burials'],
+          'Cumulative Male population change',
+          'cumulative_male_change.png',
+          cumsum=True)
 
+make_plot(['Girl Baptism', 'Girl Burials'],
+          'Cumulative Female population change',
+          'cumulative_female_change.png',
+          cumsum=True)
 
-fig, ax = make_fig_ax()
-ax.plot(groupby.Year,
-        np.cumsum(groupby['Boy Baptism'] - groupby['Boy Burials']),
-        c='C0', label='Male', marker='o')
-ax.set_xlabel('Year')
-ax.set_ylabel('Cumulative Male population change')
-ax.set_xlim(groupby.Year.min(), groupby.Year.max())
-
-fig.tight_layout(pad=0)
-fig.savefig('output/cumulative_male_change.png')
-
-
-fig, ax = make_fig_ax()
-ax.plot(groupby.Year,
-        np.cumsum(groupby['Girl Baptism'] - groupby['Girl Burials']),
-        c='C6', label='Female', marker='o')
-ax.set_xlabel('Year')
-ax.set_ylabel('Cumulative Female population change')
-ax.set_xlim(groupby.Year.min(), groupby.Year.max())
-
-fig.tight_layout(pad=0)
-fig.savefig('output/cumulative_female_change.png')
-
-fig, ax = make_fig_ax()
-ax.plot(groupby.Year,
-        np.cumsum(groupby['Boy Baptism']),
-        c='C0', label='Male', marker='o')
-ax.plot(groupby.Year,
-        np.cumsum(groupby['Girl Baptism']),
-        c='C6', label='Female', marker='o')
-ax.hlines(0, groupby.Year.min(), groupby.Year.max(),
-          color='k', alpha=0.65, linestyle='--', linewidth=1.5)
-ax.set_xlabel('Year')
-ax.set_ylabel('Cumulative Baptisms')
-ax.set_xlim(groupby.Year.min(), groupby.Year.max())
-ax.legend(loc=0)
-
-fig.tight_layout(pad=0)
-fig.savefig('output/cumulative_baptisms.png')
-
-
-fig, ax = make_fig_ax()
-ax.plot(groupby.Year,
-        np.cumsum(groupby['Boy Burials']),
-        c='C0', label='Male', marker='o')
-ax.plot(groupby.Year,
-        np.cumsum(groupby['Girl Burials']),
-        c='C6', label='Female', marker='o')
-ax.hlines(0, groupby.Year.min(), groupby.Year.max(),
-          color='k', alpha=0.65, linestyle='--', linewidth=1.5)
-ax.set_xlabel('Year')
-ax.set_ylabel('Cumulative Burials')
-ax.set_xlim(groupby.Year.min(), groupby.Year.max())
-ax.legend(loc=0)
-
-fig.tight_layout(pad=0)
-fig.savefig('output/cumulative_burials.png')
